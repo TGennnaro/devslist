b81d57d477af48fe730562c1e60a2e9a43d2f7ff
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-12 20:44:50 -0400

    react-query provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,18 +5,23 @@ import { NextUIProvider } from '@nextui-org/system';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes/dist/types';
 import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export interface ProvidersProps {
 	children: React.ReactNode;
 	themeProps?: ThemeProviderProps;
 }
 
+const queryClient = new QueryClient();
+
 export function Providers({ children, themeProps }: ProvidersProps) {
 	return (
-		<SessionProvider>
-			<NextUIProvider>
-				<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-			</NextUIProvider>
-		</SessionProvider>
+		<QueryClientProvider client={queryClient}>
+			<SessionProvider>
+				<NextUIProvider>
+					<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+				</NextUIProvider>
+			</SessionProvider>
+		</QueryClientProvider>
 	);
 }
