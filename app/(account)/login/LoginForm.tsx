99757eccd2d45f3fd0e04e0f8a4cb50090821f53
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-16 22:21:14 -0400

    login form completed

---
+++ app/(account)/login/LoginForm.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Button } from '@nextui-org/button';
+import { Input } from '@nextui-org/input';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+import { toast } from 'sonner';
+
+export default function RegisterForm() {
+	const router = useRouter();
+	const searchParams = new URLSearchParams(window?.location.search);
+	const [isLoading, setIsLoading] = useState(false);
+	async function submit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		setIsLoading(true);
+		const formData = new FormData(e.target as HTMLFormElement);
+		signIn('credentials', {
+			username: formData.get('email') as string,
+			password: formData.get('password') as string,
+			redirect: false,
+		}).then((res) => {
+			if (res?.status === 401) toast.error('Invalid username or password');
+			else if (res?.status !== 200) toast.error('An internal error occurred');
+			else if (res?.status === 200)
+				router.push(searchParams.get('callbackUrl') ?? '/');
+			setIsLoading(false);
+		});
+	}
+	return (
+		<form onSubmit={submit}>
+			<div className='mt-6'>
+				<label className='font-medium'>Email address</label>
+				<Input className='mt-2' name='email' />
+			</div>
+			<div className='mt-6'>
+				<label className='font-medium'>Password</label>
+				<Input className='mt-2' name='password' type='password' />
+			</div>
+			<Button
+				color='primary'
+				className='w-full mt-6'
+				type='submit'
+				isLoading={isLoading}
+			>
+				Login
+			</Button>
+		</form>
+	);
+}
