916d25764fd3567209674df47aac3775eb4af947
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-16 22:22:54 -0400

    fixed nav config

--- config/site.ts
+++ config/site.ts
@@ -1,22 +1,26 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "DevsList",
-  description: "The all-in-one jobs platform for developers and employers.",
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Home",
-      href: "/",
-    },
-    {
-      label: "Jobs",
-      href: "/jobs",
-    },
-  ],
+	name: 'DevsList',
+	description: 'The all-in-one jobs platform for developers and employers.',
+	navItems: [
+		{
+			label: 'Home',
+			href: '/',
+		},
+		{
+			label: 'Jobs',
+			href: '/jobs',
+		},
+	],
+	navMenuItems: [
+		{
+			label: 'Home',
+			href: '/',
+		},
+		{
+			label: 'Jobs',
+			href: '/jobs',
+		},
+	],
 };
