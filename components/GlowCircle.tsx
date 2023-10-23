827ca85f52a29bbaf3e8759fd437439f4daa2db9
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-23 18:56:08 -0400

    fixed overflow issues

--- components/GlowCircle.tsx
+++ components/GlowCircle.tsx
@@ -18,22 +18,36 @@ const circleVariants = cva(
 export default function GlowCircle({
 	top,
 	left,
+	right,
 	center = false,
-	opacity = 0.8,
+	opacity = 1.0,
 	percent = false,
-	color,
+	color = 'primary',
 }:
+	| (
+			| {
+					top: number;
+					left: number;
+					right?: undefined;
+					center?: boolean;
+					opacity?: number;
+					percent?: boolean;
+					color?: VariantProps<typeof circleVariants>['color'];
+			  }
+			| {
+					top: number;
+					left?: undefined;
+					right: number;
+					center?: boolean;
+					opacity?: number;
+					percent?: boolean;
+					color?: VariantProps<typeof circleVariants>['color'];
+			  }
+	  )
 	| {
-			top: number;
-			left: number;
-			center?: boolean;
-			opacity?: number;
-			percent?: boolean;
-			color?: VariantProps<typeof circleVariants>['color'];
-	  }
-	| {
-			top?: number;
-			left?: number;
+			top?: undefined;
+			left?: undefined;
+			right?: undefined;
 			center: true;
 			opacity?: number;
 			percent?: boolean;
@@ -45,7 +59,16 @@ export default function GlowCircle({
 			className={circleVariants({ color })}
 			style={{
 				top: center ? '50%' : `${top + unit}`,
-				left: center ? '50%' : `${left + unit}`,
+				left: center
+					? '50%'
+					: left !== undefined
+					? `${left + unit}`
+					: undefined,
+				right: center
+					? '50%'
+					: right !== undefined
+					? `${right + unit}`
+					: undefined,
 				transform: center ? 'translate(-50%, -50%)' : undefined,
 				opacity: opacity,
 			}}
