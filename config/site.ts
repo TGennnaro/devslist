b7872035938850215d2148acda27e1935d2ce67b
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-10-23 08:54:21 -0400

    Add Job Map link to navbar

--- config/site.ts
+++ config/site.ts
@@ -1,26 +1,34 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: 'DevsList',
-	description: 'The all-in-one jobs platform for developers and employers.',
-	navItems: [
-		{
-			label: 'Home',
-			href: '/',
-		},
-		{
-			label: 'Jobs',
-			href: '/jobs',
-		},
-	],
-	navMenuItems: [
-		{
-			label: 'Home',
-			href: '/',
-		},
-		{
-			label: 'Jobs',
-			href: '/jobs',
-		},
-	],
+  name: "DevsList",
+  description: "The all-in-one jobs platform for developers and employers.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Jobs",
+      href: "/jobs",
+    },
+    {
+      label: "Job Map",
+      href: "/map",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Jobs",
+      href: "/jobs",
+    },
+    {
+      label: "Job Map",
+      href: "/map",
+    },
+  ],
 };
