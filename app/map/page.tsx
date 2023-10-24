d2d9488604198a9936650509ac255b46b32aa384
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-10-24 19:56:00 -0400

    Job Map page title

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,13 @@
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Job Map',
+  description: 'Find your next job',
+};
 
 // @arcgis/core does not currently work with SSR, so we need to disable it for the map component
-const JobMapNoSSR = dynamic(() => import("@/components/JobMap"), {
+const JobMapNoSSR = dynamic(() => import('@/components/JobMap'), {
   ssr: false,
 });
 
