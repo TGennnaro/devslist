469557fce4a255aa299918a70b9f20cb835dbebc
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-10-23 20:28:13 -0400

    Single quotes

--- app/(account)/register/page.tsx
+++ app/(account)/register/page.tsx
@@ -9,35 +9,35 @@ import GlowCircle from '@/components/GlowCircle';
 export default function Page() {
   return (
     <section>
-      <GlowCircle top={20} left={35} percent color="secondary" />
-      <div className="text-center">
+      <GlowCircle top={20} left={35} percent color='secondary' />
+      <div className='text-center'>
         <h2 className={title({ size: 'sm' })}>Register an account</h2>
       </div>
-      <Card className="max-w-lg mx-auto mt-10">
-        <CardBody className="p-12">
+      <Card className='max-w-lg mx-auto mt-10'>
+        <CardBody className='p-12'>
           <RegisterForm />
-          <div className="mt-6 text-center">
-            <span className="dark:text-default-800">
+          <div className='mt-6 text-center'>
+            <span className='dark:text-default-800'>
               Already have an account?{' '}
             </span>
             <Link
-              className="font-medium text-primary hover:underline"
-              href="/login"
+              className='font-medium text-primary hover:underline'
+              href='/login'
             >
               Login
             </Link>
           </div>
-          <div className="relative mt-6">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-divider" />
+          <div className='relative mt-6'>
+            <div className='absolute inset-0 flex items-center'>
+              <div className='w-full border-t border-divider' />
             </div>
-            <div className="relative text-center">
-              <span className="px-6 font-medium bg-content1">
+            <div className='relative text-center'>
+              <span className='px-6 font-medium bg-content1'>
                 Or continue with
               </span>
             </div>
           </div>
-          <Button className="bg-[rgb(36,41,47)] hover:bg-[rgb(52,60,69)] text-white transition-background rounded-md flex gap-3 py-3 mt-6">
+          <Button className='bg-[rgb(36,41,47)] hover:bg-[rgb(52,60,69)] text-white transition-background rounded-md flex gap-3 py-3 mt-6'>
             <GithubIcon />
             GitHub
           </Button>
