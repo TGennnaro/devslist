8d69d91b32ae3fa5591bc7f20cb917040e747b8c
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-25 15:33:38 -0400

    Refactor types/index.ts to include ProfileFormEntry

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,18 @@
-import {SVGProps} from "react";
+import { SVGProps } from 'react';
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
+	size?: number;
+};
+
+export type ProfileFormEntry = {
+	email?: string | undefined;
+	firstName: string | undefined;
+	lastName: string | undefined;
+	profilePicture?: FormDataEntryValue | undefined;
+	about: string | undefined;
+	city: string | undefined;
+	country: string | undefined;
+	phoneNumber: string | undefined;
+	birthday: string | undefined;
+	[key: string]: string | FormDataEntryValue | undefined;
 };
