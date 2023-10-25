d3b3b1133595dc90195d92a1e3290a74e2fa7564
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-10-25 09:48:05 -0400

    Jobs route group and initial UI for job details

--- config/site.ts
+++ config/site.ts
@@ -1,30 +1,30 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: 'DevsList',
-	description: 'The all-in-one jobs platform for developers and employers.',
-	navItems: [
-		{
-			label: 'Jobs',
-			href: '/jobs',
-		},
-		{
-			label: 'Map',
-			href: '/map',
-		},
-	],
-	navMenuItems: [
-		{
-			label: 'Home',
-			href: '/',
-		},
-		{
-			label: 'Jobs',
-			href: '/jobs',
-		},
-		{
-			label: 'Job Map',
-			href: '/map',
-		},
-	],
+  name: 'DevsList',
+  description: 'The all-in-one jobs platform for developers and employers.',
+  navItems: [
+    {
+      label: 'Jobs',
+      href: '/jobs',
+    },
+    {
+      label: 'Map',
+      href: '/map',
+    },
+  ],
+  navMenuItems: [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Jobs',
+      href: '/jobs',
+    },
+    {
+      label: 'Job Map',
+      href: '/map',
+    },
+  ],
 };
