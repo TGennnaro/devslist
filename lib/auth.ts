8b465d30ca10d2e31b79945cc457fe73e362c3f0
Tyler Gennaro <tylergennaro10@gmail.com>
2023-10-30 08:49:13 -0400

    Add JWT authentication strategy and update session and jwt callbacks.

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,8 +4,13 @@ import bcrypt from 'bcrypt';
 import { User, Users } from '@/db/schema';
 import { db } from '@/db';
 import { eq } from 'drizzle-orm';
+import type { User as AuthUser } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 
 export const authOptions: AuthOptions = {
+	session: {
+		strategy: 'jwt',
+	},
 	providers: [
 		CredentialsProvider({
 			name: 'Email and Password',
@@ -28,11 +33,11 @@ export const authOptions: AuthOptions = {
 						const res = await bcrypt.compare(password, users[0].password);
 						if (res === true) {
 							return {
-								id: users[0].id.toString(),
+								id: users[0].id,
 								email: users[0].email,
 								firstName: users[0].firstName,
 								lastName: users[0].lastName,
-							};
+							} as AuthUser;
 						}
 					}
 					return null;
@@ -45,5 +50,35 @@ export const authOptions: AuthOptions = {
 	],
 	pages: {
 		signIn: '/login',
+	},
+	callbacks: {
+		async session({ token, session }) {
+			if (token && session?.user) {
+				session.user.id = token.id;
+				session.user.email = token.email;
+				session.user.firstName = token.firstName;
+				session.user.lastName = token.lastName;
+			}
+
+			return session;
+		},
+		async jwt({ token, user }) {
+			const dbUser = await db
+				.select()
+				.from(Users)
+				.where(eq(Users.id, token.id));
+
+			if (dbUser.length === 0) {
+				token.id = parseInt(user!.id.toString());
+				return token;
+			}
+
+			return {
+				id: dbUser[0].id,
+				email: dbUser[0].email,
+				firstName: dbUser[0].firstName,
+				lastName: dbUser[0].lastName,
+			};
+		},
 	},
 };
