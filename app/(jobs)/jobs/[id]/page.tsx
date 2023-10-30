4d2d00bdb9e290d53f3678453191a0593ed46831
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-10-29 21:40:09 -0400

    Job details page populates with DB data

--- app/(jobs)/jobs/[id]/page.tsx
+++ app/(jobs)/jobs/[id]/page.tsx
@@ -12,142 +12,166 @@ import Text from '@/components/Text';
 import { Image } from '@nextui-org/image';
 import { Metadata } from 'next';
 import { User } from '@nextui-org/user';
+import { db } from '@/db';
+import { Jobs, Company } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 const metadata: Metadata = {
   title: 'Job Posting',
   description: 'Find your next job',
 };
 
-export default function Page({ params }: { params: { id: number } }) {
-  return (
-    <>
-      <div className='flex flex-row gap-5 mb-8 items-center'>
-        <div>
-          <Image
-            isBlurred
-            alt='Company logo'
-            height={40}
-            radius='sm'
-            src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/488px-Apple_logo_black.svg.png'
-            width={40}
-            className=' p-0.5 dark:bg-gray-500'
-          />
-        </div>
-        <div>
-          <div className='text-3xl font-bold'>Job Posting {params.id}</div>
-          <div className='text-medium font-semibold'>Apple, Inc.</div>
-        </div>
-        <div>
-          <Button endContent={<Send />} variant='solid' color='primary'>
-            Apply Now
-          </Button>
-        </div>
-      </div>
+export default async function Page({ params }: { params: { id: number } }) {
+  // Check if job exists
+  async function checkExists() {
+    const result = await db
+      .select()
+      .from(Jobs)
+      .where(eq(Jobs.jobid, params.id))
+      .limit(1);
+    return result.length > 0;
+  }
 
-      <div className='flex flex-col md:flex-row gap-5'>
-        <div className='basis-1/3'>
-          <div className='flex flex-col gap-3'>
-            <div>
-              <div className='text-2xl font-semibold'>Availability</div>
-              <Text variant='body'>
-                <div className='flex items-center gap-1'>
-                  <CalendarClock /> November 1, 2023 - November 30, 2023
-                </div>
-              </Text>
+  const exists = await checkExists();
+
+  if (exists) {
+    async function fetchJob() {
+      const job = await db
+        .select()
+        .from(Jobs)
+        .where(eq(Jobs.jobid, params.id))
+        .limit(1)
+        .leftJoin(Company, eq(Jobs.companyid, Company.companyid));
+      return job[0];
+    }
+
+    const jobData = await fetchJob();
+
+    return (
+      <>
+        <div className='flex flex-row gap-5 mb-8 items-center'>
+          <div>
+            <Image
+              isBlurred
+              alt='Company logo'
+              height={40}
+              radius='sm'
+              src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/488px-Apple_logo_black.svg.png'
+              width={40}
+              className=' p-0.5 dark:bg-gray-500'
+            />
+          </div>
+          <div>
+            <div className='text-3xl font-bold'>{jobData.jobs.jobTitle}</div>
+            <div className='text-medium font-semibold'>
+              {jobData.company?.name}
             </div>
-            <Divider />
-            <div>
-              <div className='text-2xl font-semibold'>Location</div>
-              <Text variant='body'>
-                <div className='flex items-center gap-1'>
-                  <MapPin /> Cupertino, CA
-                </div>
-              </Text>
+          </div>
+          <div>
+            <Button endContent={<Send />} variant='solid' color='primary'>
+              Apply Now
+            </Button>
+          </div>
+        </div>
+
+        <div className='flex flex-col md:flex-row gap-5'>
+          <div className='basis-1/3'>
+            <div className='flex flex-col gap-3'>
+              <div>
+                <div className='text-2xl font-semibold'>Availability</div>
+                <Text variant='body'>
+                  <div className='flex items-center gap-1'>
+                    <CalendarClock />{' '}
+                    {new Date(jobData.jobs.startDate).toLocaleDateString()} -{' '}
+                    {new Date(jobData.jobs.endDate).toLocaleDateString()}
+                  </div>
+                </Text>
+              </div>
+              <Divider />
+              <div>
+                <div className='text-2xl font-semibold'>Location</div>
+                <Text variant='body'>
+                  <div className='flex items-center gap-1'>
+                    <MapPin /> {jobData.jobs.address}
+                  </div>
+                </Text>
+              </div>
+              <Divider />
+              <div>
+                <div className='text-2xl font-semibold'>Pay</div>
+                <Text variant='body'>
+                  <div className='flex items-center gap-1'>
+                    <CircleDollarSign />
+                    {jobData.jobs.salary
+                      ? '$' + jobData.jobs.salary + '/yr'
+                      : 'NONE'}
+                  </div>
+                </Text>
+              </div>
+              <Divider />
+              <div className='text-2xl font-semibold'>Job Type</div>
+              <div>
+                {jobData.jobs.jobType === 'Full-Time' ? (
+                  <Chip startContent={<Briefcase />} color='primary'>
+                    Full Time
+                  </Chip>
+                ) : jobData.jobs.jobType === 'Part-Time' ? (
+                  <Chip startContent={<Briefcase />} color='secondary'>
+                    Part Time
+                  </Chip>
+                ) : jobData.jobs.jobType === 'Internship' ? (
+                  <Chip startContent={<Briefcase />} color='success'>
+                    Internship
+                  </Chip>
+                ) : jobData.jobs.jobType === 'Freelance' ? (
+                  <Chip startContent={<Briefcase />} color='warning'>
+                    Freelance
+                  </Chip>
+                ) : (
+                  ''
+                )}
+              </div>
+              <Divider />
+              <div className='text-2xl font-semibold'>Recruiter</div>
+              <div>
+                <User
+                  name='D.B. Cooper'
+                  description='Recruiter @ Apple, Inc.'
+                  avatarProps={{
+                    src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
+                  }}
+                />
+              </div>
             </div>
-            <Divider />
-            <div>
-              <div className='text-2xl font-semibold'>Pay</div>
-              <Text variant='body'>
-                <div className='flex items-center gap-1'>
-                  <CircleDollarSign /> $120,000/yr
-                </div>
-              </Text>
-            </div>
-            <Divider />
-            <div className='text-2xl font-semibold'>Job Type</div>
-            <div>
-              <Chip startContent={<Briefcase />} color='primary'>
-                Full Time
-              </Chip>
-              <Chip startContent={<Briefcase />} color='secondary'>
-                Part Time
-              </Chip>
-              <Chip startContent={<Briefcase />} color='success'>
-                Internship
-              </Chip>
-              <Chip startContent={<Briefcase />} color='warning'>
-                Freelance
-              </Chip>
-            </div>
-            <Divider />
-            <div className='text-2xl font-semibold'>Recruiter</div>
-            <div>
-              <User
-                name='D.B. Cooper'
-                description='Recruiter @ Apple, Inc.'
-                avatarProps={{
-                  src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
-                }}
-              />
+          </div>
+          <div className='basis-2/3'>
+            <div className='flex flex-col gap-3'>
+              <div>
+                <div className='text-2xl font-semibold'>Company Overview</div>
+                <Text variant='body'>placeholder</Text>
+              </div>
+              <div>
+                <div className='text-2xl font-semibold'>Description</div>
+                <Text variant='body'>placeholder</Text>
+              </div>
+              <div>
+                <div className='text-2xl font-semibold'>Requirements</div>
+                <Text variant='body'>placeholder</Text>
+              </div>
+              <div>
+                <div className='text-2xl font-semibold'>Responsibilities</div>
+                <Text variant='body'>placeholder</Text>
+              </div>
             </div>
           </div>
         </div>
-        <div className='basis-2/3'>
-          <div className='flex flex-col gap-3'>
-            <div>
-              <div className='text-2xl font-semibold'>Company Overview</div>
-              <Text variant='body'>
-                Apple, Inc. is a world-renowned technology company known for
-                pioneering innovation and iconic, user-centric products that
-                have changed the way we live and connect.
-              </Text>
-            </div>
-            <div>
-              <div className='text-2xl font-semibold'>Description</div>
-              <Text variant='body'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                blandit turpis dui, a elementum nulla malesuada eu. Phasellus in
-                erat in dui cursus ultrices eget ut ligula. Maecenas fringilla,
-                ex non vehicula venenatis, velit massa aliquet purus, id
-                ultricies enim risus eget augue. Vestibulum imperdiet magna a
-                accumsan tempus. Duis sollicitudin sem eu odio iaculis aliquet.
-                Phasellus erat velit, rhoncus a aliquet sollicitudin, lobortis
-                quis orci. Proin commodo tortor sit amet nunc condimentum
-                rutrum. Fusce et erat quam. Maecenas erat nisi, porttitor et
-                lacus quis, eleifend venenatis quam. Nulla vel sapien a ipsum
-                pretium pharetra.
-              </Text>
-            </div>
-            <div>
-              <div className='text-2xl font-semibold'>Requirements</div>
-              <Text variant='body'>
-                Bachelor&apos;s or Master&apos;s Degree: A degree in Computer
-                Science, Software Engineering, or a related field is typically
-                required. iOS Development Expertise: Strong proficiency in iOS
-                app development using Swift and Objective-C, along with a deep
-                understanding of iOS.
-              </Text>
-            </div>
-            <div>
-              <div className='text-2xl font-semibold'>Responsibilities</div>
-              <Text variant='body'>
-                Design, develop, and maintain innovative and high-quality iOS
-                applications for Apple&apos;s ecosystem.
-              </Text>
-            </div>
-          </div>
-        </div>
+      </>
+    );
+  } else {
+    return (
+      <div className='flex items-center justify-center'>
+        Job posting does not exist.
       </div>
-    </>
-  );
+    );
+  }
 }
