d68c0b065ee841e5f959c23662b64a84e9369b7d
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-11-04 16:31:36 -0400

    Fix invisible map

--- app/(jobs)/map/page.tsx
+++ app/(jobs)/map/page.tsx
@@ -2,15 +2,19 @@ import dynamic from 'next/dynamic';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Job Map',
-  description: 'Find your next job',
+	title: 'Job Map',
+	description: 'Find your next job',
 };
 
 // @arcgis/core does not currently work with SSR, so we need to disable it for the map component
 const JobMapNoSSR = dynamic(() => import('@/app/(jobs)/map/JobMap'), {
-  ssr: false,
+	ssr: false,
 });
 
 export default function Map() {
-  return <JobMapNoSSR />;
+	return (
+		<div className='h-[100vh]'>
+			<JobMapNoSSR />
+		</div>
+	);
 }
