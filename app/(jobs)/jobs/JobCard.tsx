f694298a876f735d750b409e5349b6766bd8f6e1
Tyler Gennaro <tylergennaro10@gmail.com>
2023-11-05 23:37:06 -0500

    Add skeleton loading component to JobCard

--- app/(jobs)/jobs/JobCard.tsx
+++ app/(jobs)/jobs/JobCard.tsx
@@ -15,6 +15,7 @@ import {
 	MapPin,
 } from 'lucide-react';
 import { dateSince, timeSince } from '@/lib/utils';
+import { Skeleton } from '@nextui-org/skeleton';
 
 const MAX_DESC_LENGTH = 200;
 
@@ -39,6 +40,36 @@ const generateRatingStars = (companyRating: number) => {
 
 	return stars;
 };
+
+export function JobCardSkeleton() {
+	return (
+		<Card className='w-full hover:bg-slate-100 dark:hover:bg-slate-800'>
+			<CardHeader className='flex gap-3'>
+				<Skeleton className='w-10 h-10 rounded-md' />
+				<div className='flex flex-col gap-1'>
+					<Skeleton className='w-40 h-4 rounded-md' />
+					<Skeleton className='w-20 h-4 rounded-md' />
+					<Skeleton className='h-4 rounded-md w-52' />
+				</div>
+			</CardHeader>
+			<CardBody className='px-3 py-2'>
+				<div className='flex flex-col gap-4'>
+					<div className='flex gap-2'>
+						<Skeleton className='w-24 h-6 rounded-md' />
+						<Skeleton className='w-40 h-6 rounded-md' />
+					</div>
+					<Skeleton className='w-3/4 h-6 rounded-md' />
+
+					<Skeleton className='w-full h-32 rounded-md' />
+				</div>
+			</CardBody>
+			<CardFooter className='flex items-center justify-between'>
+				<Skeleton className='h-10 rounded-md w-28' />
+				<Skeleton className='w-24 h-6 rounded-md' />
+			</CardFooter>
+		</Card>
+	);
+}
 
 export default function JobCard({
 	id,
