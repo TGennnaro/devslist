eacadcd15cc6344412f907cded9d5c18b7ea0991
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-11-06 15:18:53 -0500

    Change GitHub OAuth button to client component

---
+++ components/GitHubOAuthButton.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Button } from '@nextui-org/button';
+import { signIn } from 'next-auth/react';
+import { GithubIcon } from 'lucide-react';
+
+export default function GitHubOAuthButton() {
+	return (
+		<Button
+			className='bg-[rgb(36,41,47)] hover:bg-[rgb(52,60,69)] text-white transition-background rounded-md flex gap-3 py-3 mt-6'
+			onClick={() => signIn('github')}
+		>
+			<GithubIcon />
+			GitHub
+		</Button>
+	);
+}
