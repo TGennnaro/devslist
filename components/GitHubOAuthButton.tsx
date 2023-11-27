039f305c536ffac5673146a991f205c8f34fe227
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-11-27 15:22:07 -0500

    GitHub OAuth button icon

--- components/GitHubOAuthButton.tsx
+++ components/GitHubOAuthButton.tsx
@@ -8,8 +8,8 @@ export default function GitHubOAuthButton() {
 		<Button
 			className='bg-[rgb(36,41,47)] hover:bg-[rgb(52,60,69)] text-white transition-background rounded-md flex gap-3 py-3 mt-6'
 			onClick={() => signIn('github', { callbackUrl: '/' })}
+			startContent={<GithubIcon />}
 		>
-			<GithubIcon />
 			GitHub
 		</Button>
 	);
