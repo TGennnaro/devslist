c54540b6c619174546d02085e03044bfb548f8cf
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-12-04 16:05:12 -0500

    Refactor profile page to include displayed and available GitHub projects

--- app/(account)/profile/page.tsx
+++ app/(account)/profile/page.tsx
@@ -1,14 +1,24 @@
 import { title } from '@/components/primitives';
 import { cn } from '@/lib/utils';
 import ProfileForm from './ProfileForm';
-import { getUser } from '@/lib/server_utils';
+import {
+	getAvailableGitHubProjects,
+	getDisplayedGitHubProjects,
+	getUser,
+} from '@/lib/server_utils';
 
 export default async function Page() {
+	const displayedProjects = await getDisplayedGitHubProjects();
+	const availableProjects = await getAvailableGitHubProjects();
 	return (
 		<div>
 			<h1 className={cn(title(), 'mb-16 block')}>Profile information</h1>
 			<hr className='mb-16 border-divider' />
-			<ProfileForm defaultValues={await getUser()} />
+			<ProfileForm
+				defaultValues={await getUser()}
+				displayedGitHubProjects={displayedProjects || []}
+				availableGitHubProjects={availableProjects || []}
+			/>
 		</div>
 	);
 }
