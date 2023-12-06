a4116d4601907aedd596fd13e0518d41388dc6d7
Tyler Gennaro <tylergennaro10@gmail.com>
2023-12-06 01:17:03 -0500

    Refactor applications table component

--- app/(account)/applications/ApplicationsTable.tsx
+++ app/(account)/applications/ApplicationsTable.tsx
@@ -8,78 +8,144 @@ import {
 	TableCell,
 } from '@nextui-org/table';
 import { Chip } from '@nextui-org/chip';
-import { Check, Loader, Undo2, X } from 'lucide-react';
-import { Button } from '@nextui-org/button';
+import { Check, Hourglass, MoreVertical, Undo2, X } from 'lucide-react';
+import { Button, ButtonProps } from '@nextui-org/button';
+import {
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownTrigger,
+} from '@nextui-org/dropdown';
+
+enum Status {
+	PENDING_REVIEW = 'Pending Review',
+	ACCEPTED = 'Accepted',
+	DISCONTINUED = 'Discontinued',
+	WITHDRAWN = 'Withdrawn',
+}
+
+const columns = [
+	{
+		key: 'position',
+		name: 'Position',
+	},
+	{
+		key: 'company',
+		name: 'Company',
+	},
+	{
+		key: 'dateApplied',
+		name: 'Date Applied',
+	},
+	{
+		key: 'lastUpdated',
+		name: 'Last Updated',
+	},
+	{
+		key: 'status',
+		name: 'Status',
+	},
+	{
+		key: 'actions',
+		name: 'Actions',
+	},
+];
+
+const rows = [
+	{
+		position: 'Software Engineer',
+		company: 'Apple, Inc.',
+		dateApplied: new Date().toLocaleDateString(),
+		lastUpdated: new Date().toLocaleDateString(),
+		status: Status.PENDING_REVIEW,
+	},
+	{
+		position: 'Intern',
+		company: 'Microsoft Corporation',
+		dateApplied: new Date().toLocaleDateString(),
+		lastUpdated: new Date().toLocaleDateString(),
+		status: Status.ACCEPTED,
+	},
+	{
+		position: 'Software Developer',
+		company: 'Microsoft Corporation',
+		dateApplied: new Date().toLocaleDateString(),
+		lastUpdated: new Date().toLocaleDateString(),
+		status: Status.DISCONTINUED,
+	},
+	{
+		position: 'Software Engineer',
+		company: 'Apple, Inc.',
+		dateApplied: new Date().toLocaleDateString(),
+		lastUpdated: new Date().toLocaleDateString(),
+		status: Status.WITHDRAWN,
+	},
+];
+
+const statusColors = {
+	[Status.PENDING_REVIEW]: 'primary',
+	[Status.ACCEPTED]: 'success',
+	[Status.DISCONTINUED]: 'danger',
+	[Status.WITHDRAWN]: 'warning',
+};
+
+const statusIcons = {
+	[Status.PENDING_REVIEW]: <Hourglass size={16} />,
+	[Status.ACCEPTED]: <Check size={16} />,
+	[Status.DISCONTINUED]: <X size={16} />,
+	[Status.WITHDRAWN]: <Undo2 size={16} />,
+};
 
 export default function ApplicationsTable() {
 	return (
-		<Table className='mt-8' aria-label='My applications' isStriped>
-			<TableHeader>
-				<TableColumn>POSITION</TableColumn>
-				<TableColumn>COMPANY</TableColumn>
-				<TableColumn>APPLIED</TableColumn>
-				<TableColumn>LAST UPDATE</TableColumn>
-				<TableColumn>STATUS</TableColumn>
-				<TableColumn>ACTIONS</TableColumn>
+		<Table className='mt-8' aria-label='My applications'>
+			<TableHeader columns={columns}>
+				{columns.map((column) => (
+					<TableColumn key={column.key} className='bg-default-100/50'>
+						{column.name}
+					</TableColumn>
+				))}
 			</TableHeader>
-			<TableBody>
-				<TableRow key='1'>
-					<TableCell>Software Engineer</TableCell>
-					<TableCell>Apple, Inc.</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>
-						<Chip color='default' variant='flat' startContent={<Loader />}>
-							Pending Review
-						</Chip>
-					</TableCell>
-					<TableCell>
-						<Button
-							variant='flat'
-							color='danger'
-							size='sm'
-							startContent={<Undo2 />}
-						>
-							Revoke
-						</Button>
-					</TableCell>
-				</TableRow>
-				<TableRow key='2'>
-					<TableCell>Intern</TableCell>
-					<TableCell>Microsoft Corporation</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>
-						<Chip color='success' variant='flat' startContent={<Check />}>
-							Accepted
-						</Chip>
-					</TableCell>
-					<TableCell>{null}</TableCell>
-				</TableRow>
-				<TableRow key='2'>
-					<TableCell>Software Developer</TableCell>
-					<TableCell>Microsoft Corporation</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>
-						<Chip color='danger' variant='flat' startContent={<X />}>
-							Rejected
-						</Chip>
-					</TableCell>
-					<TableCell>{null}</TableCell>
-				</TableRow>
-				<TableRow key='1'>
-					<TableCell>Software Engineer</TableCell>
-					<TableCell>Apple, Inc.</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>{new Date().toLocaleDateString()}</TableCell>
-					<TableCell>
-						<Chip color='warning' variant='flat' startContent={<Undo2 />}>
-							Revoked
-						</Chip>
-					</TableCell>
-					<TableCell>{null}</TableCell>
-				</TableRow>
+			<TableBody items={rows}>
+				{(item) => (
+					<TableRow key={item.position} className='my-2'>
+						<TableCell>{item.position}</TableCell>
+						<TableCell>{item.company}</TableCell>
+						<TableCell>{item.dateApplied}</TableCell>
+						<TableCell>{item.lastUpdated}</TableCell>
+						<TableCell>
+							<Chip
+								color={statusColors[item.status] as ButtonProps['color']}
+								variant='light'
+								startContent={statusIcons[item.status]}
+							>
+								{item.status}
+							</Chip>
+						</TableCell>
+						<TableCell>
+							<Dropdown>
+								<DropdownTrigger>
+									<Button
+										variant='bordered'
+										className='min-w-0 p-1 rounded-full aspect-square'
+									>
+										<MoreVertical size={16} />
+									</Button>
+								</DropdownTrigger>
+								<DropdownMenu>
+									<DropdownItem
+										key='withdraw'
+										color='warning'
+										variant='flat'
+										className='text-warning'
+									>
+										Withdraw
+									</DropdownItem>
+								</DropdownMenu>
+							</Dropdown>
+						</TableCell>
+					</TableRow>
+				)}
 			</TableBody>
 		</Table>
 	);
