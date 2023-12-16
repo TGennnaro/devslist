4a0654289216f391c987ff6efa9b84faea7ade5f
Joe <71726037+joefurmanowski@users.noreply.github.com>
2023-12-16 16:52:21 -0500

    Add company_id back to Users schema

--- db/schema.ts
+++ db/schema.ts
@@ -34,6 +34,7 @@ export const Users = mysqlTable(
 		about: text('about'),
 		dob: date('dob'),
 		isEmployer: boolean('is_employer').default(false),
+		companyId: int('company_id'),
 		githubID: text('github_id'),
 		githubAccessToken: text('github_access_token'),
 		gender: boolean('gender'),
