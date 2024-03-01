ec31fed6f79439d3eb97f6cfaccfeab5a55be238
Joe <71726037+joefurmanowski@users.noreply.github.com>
2024-02-29 21:18:16 -0500

    Drop old migrations

--- drizzle/0001_shocking_rhino.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "roles" TO "isEmployer";--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "isEmployer" SET DATA TYPE boolean;
--- drizzle/0002_married_white_tiger.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "isEmployer" TO "is_employer";--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "is_employer" SET DEFAULT false;
