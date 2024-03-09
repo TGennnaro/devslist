767e67969da6d8c54a0be72dc40cb7e062e54d65
Joe <71726037+joefurmanowski@users.noreply.github.com>
2024-03-09 15:14:23 -0500

    Use unread messages context in inbox

--- app/(account)/inbox/page.tsx
+++ app/(account)/inbox/page.tsx
@@ -4,7 +4,6 @@ import { desc, eq } from 'drizzle-orm';
 import { Messages, Users } from '@/db/schema';
 import Inbox from './Inbox';
 import { getUser } from '@/lib/server_utils';
-import Text from '@/components/Text';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -22,14 +21,9 @@ export default async function InboxPage() {
 			.leftJoin(Users, eq(Messages.fromId, Users.id))
 			.orderBy(desc(Messages.timeSent));
 
-		const unread = allMessages.filter((m) => m.messages.isOpened === 0).length;
-
 		return (
 			<>
 				<h1 className={title()}>Inbox</h1>
-				<Text variant='body' className='mt-3'>
-					{allMessages.length} total messages ({unread} unread)
-				</Text>
 				<div className='mt-5'>
 					<Inbox messages={allMessages} />
 				</div>
