44424a845cd9ce186b40b392be991cbd113bedff
Tyler Gennaro <tylergennaro10@gmail.com>
2024-03-29 11:57:21 -0400

    fixed company list dropdown on job posting form

--- lib/models/company.ts
+++ lib/models/company.ts
@@ -5,16 +5,13 @@ import { eq, count } from 'drizzle-orm';
 export async function getCompaniesByUser(
 	userId: number,
 	sorted: boolean = false,
+	paginated: boolean = false,
 	page: number = 0
 ) {
 	const LIMIT = 10;
 	// only use orderBy if sorted is true
-	let query = db
-		.select()
-		.from(Company)
-		.where(eq(Company.userId, userId))
-		.offset((page - 1) * LIMIT)
-		.limit(LIMIT);
+	let query = db.select().from(Company).where(eq(Company.userId, userId));
+	if (paginated) query.offset((page - 1) * LIMIT).limit(LIMIT);
 	if (sorted) query.orderBy(Company.name);
 	let total = db
 		.select({
