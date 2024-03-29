ae3e44a8628c5ad2c6e26ce70586e4a68b6d8ff7
Joe <71726037+joefurmanowski@users.noreply.github.com>
2024-03-29 11:52:09 -0400

    Change DB provider from PlanetScale to self-hosted MySQL

--- db/index.ts
+++ db/index.ts
@@ -1,19 +1,20 @@
-import { connect } from '@planetscale/database';
-import { drizzle } from 'drizzle-orm/planetscale-serverless';
-import { migrate } from 'drizzle-orm/planetscale-serverless/migrator';
+import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
 
-const connection = connect({
+const connection = await mysql.createConnection({
 	host: process.env.DATABASE_HOST,
-	username: process.env.DATABASE_USERNAME,
+	user: process.env.DATABASE_USERNAME,
+	database: process.env.DATABASE_NAME,
 	password: process.env.DATABASE_PASSWORD,
 });
 
 export const db = drizzle(connection);
 
 // async function run() {
-// 	await migrate(db, { migrationsFolder: 'drizzle' }).catch((err) => {
-// 		console.error('Migration error: ', err);
-// 	});
+//  await migrate(db, { migrationsFolder: 'drizzle' }).catch((err) => {
+//      console.error('Migration error: ', err);
+//  });
 // }
 
 // run();
